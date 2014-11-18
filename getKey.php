0bfaf653ae57972776c9a797a2aba5366e25944d
Pedro Moreira <pmoreira.estg@gmail.com>
2014-11-18 14:53:39 +0000

    fim da aula de 18.11.2014

--- getKey.php
+++ getKey.php
@@ -1,12 +1,15 @@
 <?php
+	header("Access-Control-Allow-Origin: *");
 	include "CLotto.php";
 	// defeito
 	
-	$f = "json";
+	$f = "error";
 	
 	if(isset($_GET["format"])) {
 		if ($_GET["format"] == "xml") {
 			$f = "xml";
+		} else if ($_GET["format"] == "json") {
+			$f = "json";
 		}
 	}
 	
@@ -15,9 +18,11 @@
 	if ($f == "xml") {
 		header("Content-Type:application/xml");
 		echo $newKey->asXML();
-	} else {
+	} else if ($f == "xml") {
 		header("Content-Type:application/json");
 		echo $newKey->asJSON();	
+	} else {
+		http_response_code(400);
 	}
 	
 ?>
